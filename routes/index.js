9ec4dff406b5a81e788955d0263090b6a557bd21
Nidhin P Pramod <kichum386@gamil.com>
2022-06-02 17:54:15 +0530

    Nodejs Express Basics Studied

--- routes/index.js
+++ routes/index.js
@@ -3,7 +3,12 @@ var router = express.Router();
 
 /* GET home page. */
 router.get('/', function(req, res, next) {
-  res.render('index', { title: 'Express' });
+
+  const value=["Nidhin","Sai","Ashwin"]
+
+  const obj={admin:true,name:"Nihdin",comments:{comment:"this is me Admin",date:"07-02-2022"}}
+
+  res.render('index', {value,obj});
 });
 
 module.exports = router;
